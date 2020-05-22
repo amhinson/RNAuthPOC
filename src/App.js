884a9d99606205695b659d8c011449ffebf3673a
Alex Hinson <alexmhinson@gmail.com>
2020-05-22 08:56:01 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {
-  Authenticator,
-  SignUp,
-  withAuthenticator,
-} from 'aws-amplify-react-native';
+import {Authenticator, SignUp} from 'aws-amplify-react-native';
 import LoggedInContent from './LoggedInContent';
 import CustomSignIn from './CustomSignIn';
 
-// const App = () => {
-//   return (
-//     <Authenticator
-//       // theme={customTheme}
-//       components={{
-//         // signIn: <CustomSignIn />,
-//         signUp: <SignUp headerText="Custom Sign Up Header" />,
-//       }}>
-//       <LoggedInContent />
-//     </Authenticator>
-//   );
-// };
 const App = () => {
-  return <LoggedInContent />;
+  return (
+    <Authenticator
+      theme={customTheme}
+      components={{
+        signIn: <CustomSignIn />,
+        signUp: <SignUp headerText="Custom Sign Up Header" />,
+      }}>
+      <LoggedInContent />
+    </Authenticator>
+  );
 };
 
 const customTheme = StyleSheet.create({
@@ -29,5 +22,3 @@ const customTheme = StyleSheet.create({
     backgroundColor: 'greenyellow',
   },
 });
-
-export default withAuthenticator(App);
